5748f2c0ec47772f495f5a1899ebb47f9a599f94
Maxwell Lafontant <maxwell@maxwellrlafontant.com>
2020-11-03 18:17:43 -0600

    Hello World in NetBeans

---
+++ Classwork/HelloWorld/src/main/java/com/mrl/helloworld/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * @author Maxwell R. Lafontant
+ * email: maxwell@MaxwellRLafontant.com
+ * date: 11/2/2020
+ * purpose: My first Hello World program in an IDE
+ */
+package com.mrl.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maxwell!");
+    }
+}
